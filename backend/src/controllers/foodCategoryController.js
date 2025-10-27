10215335a5f767b38e404d0643a9e7cec596057b
Zolo517 <dagiirazzoljargal@gmail.com>
2025-10-27 12:08:37 +0800

    deployment test

--- backend/src/controllers/foodCategoryController.js
+++ backend/src/controllers/foodCategoryController.js
@@ -5,7 +5,15 @@ export const createFoodCategory = async (req, res) => {
     const data = await FoodCategory.create({
       categoryName: req.body.categoryName,
     });
-    res.status(200).send({message: "successfully", data:data})
+    res.status(200).send({ message: "successfully", data: data });
+  } catch (error) {
+    console.error(error);
+  }
+};
+export const getfoodCategories = async (req, res) => {
+  try {
+    const data = await FoodCategory.find();
+    res.status(200).send(data);
   } catch (error) {
     console.error(error);
   }
--- backend/src/routers/foodCategoryRoute.js
+++ backend/src/routers/foodCategoryRoute.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { createFoodCategory } from "../controllers/foodCategoryController.js";
+import {
+  createFoodCategory,
+  getfoodCategories,
+} from "../controllers/foodCategoryController.js";
 
 export const categoryRouter = Router();
 
-categoryRouter.post("/", createFoodCategory);
+categoryRouter.post("/", createFoodCategory).get("/", getfoodCategories);
