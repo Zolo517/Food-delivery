c3f8ba4a521da00163114a39abafb30c3d70acad
Zolo517 <dagiirazzoljargal@gmail.com>
2025-11-03 10:16:17 +0800

    added admin page sections

---
+++ frontend/src/app/adminpage/feature/FoodMenuSection.tsx
@@ -0,0 +1,13 @@
+import { AllDishes } from "@/components/AllDishes";
+import { Avatar } from "@/components/ui/avatar";
+
+export const FoodMenuSection = () => {
+  return (
+    <div className="py-6 px-10 gap-6 ">
+      <div className="flex justify-end">
+        <Avatar />
+      </div>
+      <AllDishes />
+    </div>
+  );
+};
